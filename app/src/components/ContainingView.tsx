8a239ab74de3a4061a292b5f88bad7a1aca82987
Krishan Patel <krishan711@gmail.com>
2025-02-28 18:45:59 +0000

    Remove loading

--- app/src/components/ContainingView.tsx
+++ app/src/components/ContainingView.tsx
@@ -15,7 +15,6 @@ export function ContainingView(props: IContainingViewProps): React.ReactElement 
   const chainId = useWeb3ChainId();
   const account = useWeb3Account();
   const loginSignature = useWeb3LoginSignature();
-  const isLoading = chainId === undefined || account === undefined || loginSignature === undefined;
   const isLoggedIn = chainId != null && account != null && loginSignature != null;
   const [isMenuOpen, setIsMenuOpen] = React.useState<boolean>(false);
   const location = useLocation();
@@ -38,51 +37,45 @@ export function ContainingView(props: IContainingViewProps): React.ReactElement 
 
   return (
     <Box className={props.className} zIndex={1} position='absolute' isFullWidth={true} isFullHeight={true}>
-      {isLoading ? (
-        <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} contentAlignment={Alignment.Center} childAlignment={Alignment.Center}>
-          <LoadingIndicator isLarge={true} />
-        </Stack>
-      ) : (
-        <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} contentAlignment={Alignment.Start} childAlignment={Alignment.Center}>
-          {isConnected && (
-            <Box variant='navBar' zIndex={999} shouldClipContent={true} isFullWidth={true}>
-              <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} isFullHeight={true} shouldAddGutters={true}>
-                <LinkBase target='/'>
-                  <Image source='/assets/icon.png' alternativeText='logo' height='32px' fitType='contain' />
-                </LinkBase>
-                {!isChatPage && (
-                  <Text variant='bold-large'>FlashBlock Speed Challenge</Text>
-                )}
-                <Stack.Item growthFactor={1} shrinkFactor={1} shouldShrinkBelowContentSize={true}>
-                  <ResponsiveHidingView hiddenAbove={ScreenSize.Medium}>
-                    <Box shouldClipContent={true}>
-                      <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
-                        <Button variant='navBarLocation' text={getPageName()} onClicked={onMenuClicked} />
-                      </Stack>
-                    </Box>
-                  </ResponsiveHidingView>
-                </Stack.Item>
-                <Stack.Item growthFactor={1} shrinkFactor={1} />
-                {isLoggedIn && (
-                  <ResponsiveHidingView hiddenBelow={ScreenSize.Medium}>
-                    <AccountView address={account.address} />
-                  </ResponsiveHidingView>
-                )}
-                {isLoggedIn && (
-                  <ResponsiveHidingView hiddenAbove={ScreenSize.Medium}>
-                    <IconButton icon={<KibaIcon iconId='ion-menu-outline' />} label='Menu' onClicked={onMenuClicked} />
-                  </ResponsiveHidingView>
-                )}
-              </Stack>
-            </Box>
-          )}
-          <Stack.Item growthFactor={1} shrinkFactor={0} shouldShrinkBelowContentSize={false}>
-            <Box variant='empty' shouldClipContent={true} isScrollableVertically={false} isFullWidth={true}>
-              {props.children}
-            </Box>
-          </Stack.Item>
-        </Stack>
-      )}
+      <Stack direction={Direction.Vertical} isFullWidth={true} isFullHeight={true} contentAlignment={Alignment.Start} childAlignment={Alignment.Center}>
+        {isConnected && (
+          <Box variant='navBar' zIndex={999} shouldClipContent={true} isFullWidth={true}>
+            <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center} isFullHeight={true} shouldAddGutters={true}>
+              <LinkBase target='/'>
+                <Image source='/assets/icon.png' alternativeText='logo' height='32px' fitType='contain' />
+              </LinkBase>
+              {!isChatPage && (
+                <Text variant='bold-large'>FlashBlock Speed Challenge</Text>
+              )}
+              <Stack.Item growthFactor={1} shrinkFactor={1} shouldShrinkBelowContentSize={true}>
+                <ResponsiveHidingView hiddenAbove={ScreenSize.Medium}>
+                  <Box shouldClipContent={true}>
+                    <Stack direction={Direction.Horizontal} childAlignment={Alignment.Center} contentAlignment={Alignment.Center}>
+                      <Button variant='navBarLocation' text={getPageName()} onClicked={onMenuClicked} />
+                    </Stack>
+                  </Box>
+                </ResponsiveHidingView>
+              </Stack.Item>
+              <Stack.Item growthFactor={1} shrinkFactor={1} />
+              {isLoggedIn && (
+                <ResponsiveHidingView hiddenBelow={ScreenSize.Medium}>
+                  <AccountView address={account.address} />
+                </ResponsiveHidingView>
+              )}
+              {isLoggedIn && (
+                <ResponsiveHidingView hiddenAbove={ScreenSize.Medium}>
+                  <IconButton icon={<KibaIcon iconId='ion-menu-outline' />} label='Menu' onClicked={onMenuClicked} />
+                </ResponsiveHidingView>
+              )}
+            </Stack>
+          </Box>
+        )}
+        <Stack.Item growthFactor={1} shrinkFactor={0} shouldShrinkBelowContentSize={false}>
+          <Box variant='empty' shouldClipContent={true} isScrollableVertically={false} isFullWidth={true}>
+            {props.children}
+          </Box>
+        </Stack.Item>
+      </Stack>
     </Box>
   );
 }
